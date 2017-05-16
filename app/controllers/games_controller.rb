864117299429e724ba137bafb1c2779bd08d2ef2
jwmunn <contact@justinmunn.co>
2017-05-15 18:29:22 -0800

    Adjusting tabs vs spaces for code-climate.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,11 @@
 class GamesController < ApplicationController
-	def index
-		@games = Game.all
-	end
+  def index
+    @games = Game.all
+  end
 
-	def new
-	end
+  def new end
 
-	def create
-	end
+  def create end
 
-	def show
-	end
+  def show end
 end
