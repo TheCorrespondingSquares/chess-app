13e3b7c4a0016d6caf161f779616a7f8c530020a
jwmunn <contact@justinmunn.co>
2017-05-15 18:25:37 -0800

    Code-climate tweaks.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,14 @@
 class GamesController < ApplicationController
-    
-    def index
-        @games = Game.all
-    end
-    
-    def new
-    end
-    
-    def create
-    end
-    
-    def show
-    end
-    
+	def index
+		@games = Game.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
 end
