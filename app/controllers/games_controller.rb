ba9ecc5a8f09282c90501a68dc974316e8a77a11
Miguel Bustamante <phatbastard81@yahoo.com>
2017-05-16 16:41:56 +0000

    added CRUD functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,9 +8,16 @@ class GamesController < ApplicationController
     end
     
     def create
+        @game = Game.create(game_params)
     end
     
     def show
+        @game = Game.find(params[:id])
     end
     
+private 
+    
+    def game_params
+        params.require(:game).permit(:name)
+    end
 end
