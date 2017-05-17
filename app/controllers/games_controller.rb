8e2b00f6b46df1f7c5301a4125b804ecd0d1c3bb
Miguel Bustamante <phatbastard81@yahoo.com>
2017-05-17 01:00:53 +0000

    adjusted indentation errors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,23 +1,24 @@
 class GamesController < ApplicationController
     
-    def index
-        @games = Game.all
-    end
+  def index
+    @games = Game.all
+  end
     
-    def new
-    end
+  def new
+    @game = Game.new
+  end
     
-    def create
+  def create
         @game = Game.create(game_params)
-    end
+  end
     
-    def show
-        @game = Game.find(params[:id])
-    end
-    
-private 
-    
-    def game_params
-        params.require(:game).permit(:name)
-    end
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private 
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
