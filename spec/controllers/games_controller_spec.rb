aad5810213b0e78a41037733a9d2051221d5513d
Miguel Bustamante <phatbastard81@yahoo.com>
2017-05-17 02:41:21 +0000

    indentations

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GamesController, type: :controller do
   describe 'games#index action' do
     it 'should successfully show the page' do
       get :index
-    expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
     end
   end
 end
