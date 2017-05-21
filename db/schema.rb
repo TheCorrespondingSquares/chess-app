5f6303d973c85397018d8e3f41e46c01de814614
fistoriza <fistoriza@me.com>
2017-05-21 08:20:35 +0000

    run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517024931) do
+ActiveRecord::Schema.define(version: 20170521054028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "name"
+    t.boolean  "public_game"
   end
 
   create_table "pieces", force: :cascade do |t|
