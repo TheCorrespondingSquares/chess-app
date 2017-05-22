afddb97150dd7089993a2eb5ada8d08cd5310f4d
fistoriza <fistoriza@me.com>
2017-05-22 18:52:34 +0000

    added STI models

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,15 @@
 class Piece < ApplicationRecord
 	belongs_to :game
+	self.inheritance_column = :name
+
+	scope :kings, -> { where(name: 'King') }
+	scope :queens, -> { where(name: 'Queen') }
+	scope :bishops, -> { where(name: 'Bishop') }
+	scope :knights, -> { where(name: 'Knight') }
+	scope :rooks, -> { where(name: 'Rook') }
+	scope :pawns, -> { where(name: 'Pawn') }
+
+	def self.names
+		%w(King Queen Bishop Knight Rook Pawn)
+	end
 end
