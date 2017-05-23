907466a1cf05d9cf3dd1e0ac69d26b00e820fe36
Nikhil <nnadkarny@gmail.com>
2017-05-23 20:40:41 +0000

    is_occupied? tests

--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+  # A default queen piece, due to her range of movement (useful for tests)
   factory :piece do
+    name: "queen"
+    color: "black"
+    x_pos: 3
+    y_pos: 3
+    game_id: 1
+    captured: false
   end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "is_occupied? method" do
+    it "should return true if piece exists at coordinates" do
+      game = FactoryGirl.create(:game)
+      piece = FactoryGirl.create(:piece, game_id: game.id)
+
+      is_occupied?(piece.x_pos, piece.y_pos)
+      expect(response).to eq(true)
+    end
+
+    it "should return false if no piece exists at coordinates" do
+      game = FactoryGirl.create(:game)
+      piece = FactoryGirl.create(:piece, game_id: game.id)
+      empty_x = piece.x_pos - 1
+
+      is_occupied?(empty_x, piece.y_pos)
+      expect(response).to eq(false)
+    end
+  end
+
+  # describe "is_obstructed? method" do
+  #   it "should return error message if piece is a knight"
+
+  #   end
+
+  #   it "should return true for vertical obstruction" do
+  #     game = FactoryGirl.create(:game)
+  #     piece_moves = FactoryGirl.create(:piece, game_id: game.id)
+  #     piece_obstructs = FactoryGirl.create(:piece, name: "pawn", x_pos: piece_moves.x_pos, y_pos: piece_moves.y_pos + 2, game_id: game.id)
+
+  #     piece_moves.is_obstructed?(piece_moves.x_pos, piece_moves.y_pos + 4)
+  #     expect(response).to eq(true)
+  #   end
+
+  #   it "should return false for no veritcal obstruction" do
+
+  #   end
+
+  #   it "should return true for horizontal obstruction" do
+  #     game = FactoryGirl.create(:game)
+  #     piece_moves = FactoryGirl.create(:piece, game_id: game.id)
+  #     piece_obstructs = FactoryGirl.create(:piece, name: "pawn", x_pos: piece_moves.x_pos - 2, y_pos: piece_moves.y_pos, game_id: game.id)
+
+  #     piece_moves.is_obstructed?(piece_moves.x_pos - 3, piece_moves.y_pos)
+  #     expect(response).to eq(true)
+  #   end
+
+  #   it "should return false for no horizontal obstruction" do
+
+  #   end
+
+  #   it "should return true for diagonal obstruction" do
+  #     game = FactoryGirl.create(:game)
+  #     piece_moves = FactoryGirl.create(:piece, game_id: game.id)
+  #     piece_obstructs = FactoryGirl.create(:piece, name: "pawn", x_pos: piece_moves.x_pos + 2, y_pos: piece_moves.y_pos + 2, game_id: game.id)
+
+  #     piece_moves.is_obstructed?(piece_moves.x_pos + 4, piece_moves.y_pos + 4)
+  #     expect(response).to eq(true)
+  #   end
+
+  #   it "should return false for no diagonal obstruction" do
+
+  #   end
+  # end
+
 end
