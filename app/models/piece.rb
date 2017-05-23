219a63a69ead2d524484f84605bdf0601e76aadb
kboygit <heybisayangdako@gmail.com>
2017-05-23 21:23:02 +0000

    fixed alignment

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,15 +1,15 @@
 class Piece < ApplicationRecord
 	belongs_to :user
 	belongs_to :game
-	
+
 	self.inheritance_column = :name
 
-	scope :kings, -> { where(name: 'King') }
-	scope :queens, -> { where(name: 'Queen') }
+	scope :kings,		-> { where(name: 'King') }
+	scope :queens, 	-> { where(name: 'Queen') }
 	scope :bishops, -> { where(name: 'Bishop') }
 	scope :knights, -> { where(name: 'Knight') }
-	scope :rooks, -> { where(name: 'Rook') }
-	scope :pawns, -> { where(name: 'Pawn') }
+	scope :rooks,		-> { where(name: 'Rook') }
+	scope :pawns, 	-> { where(name: 'Pawn') }
 
 	def self.names
 		%w(King Queen Bishop Knight Rook Pawn)
