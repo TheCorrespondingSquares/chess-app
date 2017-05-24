10f2c142f0a982b277a11ea5a302e7b3e66a5461
Miguel Bustamante <phatbastard81@yahoo.com>
2017-05-24 23:31:22 +0000

    adding scope to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,7 @@
 class Game < ApplicationRecord
 	has_many :pieces
+	
+	scope :available, ->{ where(available: true)}
 
 	delegate :kings, :queens, :bishops, :knights, :rooks, :pawns, to: :pieces
 end
