0dbf7402d426931d497fc404316293b9c8ef0382
Miguel Bustamante <phatbastard81@yahoo.com>
2017-05-26 20:32:22 +0000

    adjusted available method

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ApplicationRecord
 	has_many :pieces
 	
 	def self.available
-	  where(available: true) if user.count < 2 && :private != true 
+	  where(available: true) if :user_id.count == 1 && :private != true
 	end
 
 	delegate :kings, :queens, :bishops, :knights, :rooks, :pawns, to: :pieces
