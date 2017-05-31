b154144ccea1a6dc0af3986ac830bf617f71db28
Fisto Riza Satianto <fistoriza@me.com>
2017-05-31 08:39:43 +0000

    building rspec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'games#show action' do
+  	it 'should successfully show the pieces which belong to the game' do
+
+  		expect(@pieces.first).to be
+  	end
+  end	
 end
