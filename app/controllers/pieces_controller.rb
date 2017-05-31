2a371b5640ba70e15002e24efd93460362aae6f3
Fisto Riza Satianto <fistoriza@me.com>
2017-05-31 17:06:25 +0000

    create a link to a piece page

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,11 +2,27 @@ class PiecesController < ApplicationController
 
 	def show
 		@piece = Piece.find(params[:id])
+		@game = @piece.game
+	end
+
+	def update
+		@piece = Piece.find(params[:id])
+		@game = @piece.game
+		
+		return render_not_found if @piece.blank?
+
+		@piece.update_attributes(piece_params)
+
+		if @piece.valid?
+			redirect_to game_path(@game)
+		else
+			return render :edit, status: :unprocessable_entity
+		end
 	end
 
   private
 
-  def game_params
-    params.require(:piece).permit(:name, :x_pos, :y_pos, :captured)
+  def piece_params
+    params.permit(:name, :x_pos, :y_pos, :captured, :game_id)
   end
 end
