ce084b32cd34a359dfc9e9cf411d1cf3895d3fbe
Fisto Riza Satianto <fistoriza@me.com>
2017-06-04 15:16:50 +0000

    refactored the code to pass the test

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,16 +12,20 @@ class Game < ApplicationRecord
 	  8.times do |i|
 	  	Pawn.create(color: 'Black', game_id: self.id, x_pos: i, y_pos: 1)
 	  end
+	  
 		big_pieces.each_with_index do |piece, i|
 			Piece.create(name: piece, color: 'Black', game_id: self.id, x_pos: i, y_pos: 0)
 		end
 	  # ------ White pieces ----------
+		
 		8.times do |i|
 	  	Pawn.create(color: 'White', game_id: self.id, x_pos: i, y_pos: 6)
 	  end
+
 		big_pieces.each_with_index do |piece, i|
 			Piece.create(name: piece, color: 'White', game_id: self.id, x_pos: i, y_pos: 7)
 		end
+
 	end
 
 	def big_pieces
--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe PiecesController, type: :controller do
   let!(:piece) { FactoryGirl.create(:piece, game_id: game.id, color: 'black') }
 
 	describe 'pieces#show action' do
-  	it 'should successfully show the page of the game' do
+    it 'should successfully show the page of the game' do
   		get :show, params: { game_id: game.id, id: piece.id }
   		expect(response).to have_http_status(:success)
   		expect(piece.color).to eq('black')
-  	end	
+  	end
   end
 
   describe 'pieces#update action' do
