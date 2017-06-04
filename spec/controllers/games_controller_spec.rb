2916dc895e46806083e37af379532ef3ce01dda0
Miguel Bustamante <phatbastard81@yahoo.com>
2017-06-04 05:36:00 +0000

    attempting to fix tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'games#show action' do
+  	let!(:user) { FactoryGirl.create(:user) }
+  	let!(:game) { FactoryGirl.create(:game, white_player_id: user.id) }
+  	let!(:piece) { FactoryGirl.create(:piece, game_id: game.id, color: 'black') }
+  	let!(:piece2) { FactoryGirl.create(:piece, game_id: game.id, color: 'white') }
+  	
+  	it 'should successfully show the page of the game' do
+  		get :show, params: { id: game.id }
+  		expect(response).to have_http_status(:success)
+  	end
+
+  	it 'should return the number and the correct piece' do
+  		get :show, params: { id: game.id }
+  		@pieces = game.pieces
+  		expect(@pieces.count).to eq 2
+  		expect(@pieces.first.color).to eq('black')
+  	end
+  end	
 end
--- spec/factories/pieces.rb
+++ spec/factories/pieces.rb
@@ -2,10 +2,9 @@ FactoryGirl.define do
   # A default queen piece, due to her range of movement (useful for tests)
   factory :piece do
     name "Queen"
-    color "black"
+    # color "black"
     x_pos 1
     y_pos 1
-    game_id 1
     captured false
   end
 end
