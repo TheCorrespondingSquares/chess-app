65e83a7adf64eaf73988ddb43d02a8f3b97a0008
Fisto Riza Satianto <fistoriza@me.com>
2017-06-06 05:18:44 +0000

    fixed the populate board method so it doesnt populate 64 pieces

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,7 +12,7 @@ class GamesController < ApplicationController
 
   def create
     @game = Game.create(game_params)
-    @game.populate_board!
+    # @game.populate_board!
     redirect_to game_path(@game)
   end
 
