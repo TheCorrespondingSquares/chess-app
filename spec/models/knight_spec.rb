548204d96af788eb3054b426fe8cc51e6cc332d9
Nikhil <nnadkarny@gmail.com>
2017-06-08 16:23:38 +0000

    Spacing fixes in knight_spec and indentation fix in king_spec

--- spec/models/knight_spec.rb
+++ spec/models/knight_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Knight, type: :model do
   before(:each) { game.pieces.destroy_all }
 
   describe "#knight_valid_move?" do
-    subject (:knight_valid_move?) {knight.knight_valid_move?(to_x, to_y)}
+    subject(:knight_valid_move?) { knight.knight_valid_move?(to_x, to_y) }
     
     let(:knight) {FactoryGirl.create(:knight, x_pos: 2, y_pos: 0, color: 'White', game_id: game.id)}
     
