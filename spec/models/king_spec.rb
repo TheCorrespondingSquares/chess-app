3a4e7b7eb3dc80e5b96683f75f423b9a459b2694
Nikhil <nnadkarny@gmail.com>
2017-06-08 13:07:16 +0000

    created separate _spec.rb files for individual pieces

---
+++ spec/models/king_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe King, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:game) { FactoryGirl.create(:game, black_player_id: user.id)}
+  before(:each) { game.pieces.destroy_all }
+
+describe "#king_valid_move?" do
+    subject(:king_valid_move?) { king.king_valid_move?(destination_x, destination_y) }
+
+    let(:king) { FactoryGirl.create(:king, color: "Black", x_pos: 4, y_pos: 0, game_id: game.id) }
+
+    context 'for horizontal move' do
+      let(:destination_y) { king.y_pos }
+
+      context 'when valid' do
+        let(:destination_x) { 3 }
+        
+        it { is_expected.to eq(true) }
+      end
+
+      context 'when not valid' do
+        let(:destination_x) { 6 }
+
+        it { is_expected.to eq(false) }
+      end
+    end
+
+    context 'for vertical move' do
+      let(:destination_x) { king.x_pos }
+
+      context 'when valid' do
+        let(:destination_y) { 1 }
+
+        it { is_expected.to eq(true) }
+      end
+
+      context 'when not valid' do
+        let(:destination_y) { 5 }
+
+        it { is_expected.to eq(false) }
+      end
+    end
+
+    context 'for diagonal move' do
+      context 'when valid' do
+        let(:destination_x) { 3 }
+        let(:destination_y) { 1 }
+
+        it { is_expected.to eq(true) }
+      end
+
+      context 'when not valid' do
+        let(:destination_x) { 1 }
+        let(:destination_y) { 3 }
+
+        it { is_expected.to eq(false) }
+      end
+    end
+  end
+
+end  
---
+++ spec/models/knight_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Knight, type: :model do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:game) { FactoryGirl.create(:game, black_player_id: user.id)}
+  before(:each) { game.pieces.destroy_all }
+
+  describe "#knight_valid_move?" do
+    subject (:knight_valid_move?) {knight.knight_valid_move?(to_x, to_y)}
+    
+    let(:knight) {FactoryGirl.create(:knight, x_pos: 2, y_pos: 0, color: 'White', game_id: game.id)}
+    
+    context 'for valid move' do
+      context '"tall"' do
+        let(:to_x) { 3 }
+        let(:to_y) { 2 }
+        
+        it { is_expected.to eq(true) }
+      end
+
+      context '"wide"' do
+        let(:to_x) { 0 }
+        let(:to_y) { 1 }
+
+        it { is_expected.to eq(true) }
+      end
+    end
+
+    context 'for invalid move' do
+      let(:to_x) { 4 }
+      let(:to_y) { 2 }
+
+      it { is_expected.to eq(false) }
+    end
+  end
+
+end    
