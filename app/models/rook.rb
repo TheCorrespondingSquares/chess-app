694535e018bdaa87ebd06d72121c9bf2b7e3d1ab
Miguel Bustamante <phatbastard81@yahoo.com>
2017-06-09 04:20:26 +0000

    begin rook logic in rook model

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -8,4 +8,21 @@ after_create :icon
       '&#9820;'
     end
   end
+  
+  def rook_valid_move?(to_x, to_y)
+    return false if self.is_obstructed(to_x, to_y)
+    rook.horizantal_move?(to_y) || rook.vertical_move?(to_x)
+  end
+
+  private
+  
+  def horizantal_move?(to_y)
+    if to_y != starting_point_y
+    end
+  end
+  
+  def vertical_move?(to_x)
+    if to_y != starting_point_x
+    end
+  end
 end
