b283ffbd9a63880a8a7898e61a4671d840da5bf2
Nikhil <nnadkarny@gmail.com>
2017-06-17 17:54:40 +0000

    Updated king.rb to match Master

--- app/models/king.rb
+++ app/models/king.rb
@@ -10,21 +10,7 @@ after_create :icon
   end
 
   def valid_move?(to_x, to_y)
-    king_move_horizontal?(to_x) || king_move_vertical?(to_y) || king_move_diagonal?(to_x, to_y)
-  end
-
-  private
-
-  def king_move_horizontal?(to_x)
-    right_or_left(to_x) != 0 && to_x == starting_point_x + right_or_left(to_x)
-  end
-
-  def king_move_vertical?(to_y)
-    up_or_down(to_y) != 0 && to_y == starting_point_y + up_or_down(to_y)
-  end
-
-  def king_move_diagonal?(to_x, to_y)
-    king_move_horizontal?(to_x) && king_move_vertical?(to_y)
+    horizontal_move_one_square?(to_x) || vertical_move_one_square?(to_y) || diagonal_move_one_square?(to_x, to_y)
   end
 
 end
