d34ef894846863ef6c3aeceb1c5b2f26ba575895
Nikhil <nnadkarny@gmail.com>
2017-06-17 18:03:10 +0000

    Updated queen.rb (removed private methods, no longer needed)

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -14,18 +14,4 @@ after_create :icon
     horizontal_move_only?(to_x, to_y) || vertical_move_only?(to_x, to_y) || diagonal_move?(to_x, to_y)
   end
 
-  private
-
-  def queen_move_horizontal?(to_x)
-    right_or_left(to_x) != 0
-  end
-
-  def queen_move_vertical?(to_y)
-    up_or_down(to_y) != 0
-  end
-
-  def queen_move_diagonal?(to_x, to_y)
-    (starting_point_x - to_x).abs == (starting_point_y - to_y).abs
-  end  
-
 end
