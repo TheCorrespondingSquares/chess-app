0f3a960c48783477570317e0011486e47220dfb7
Miguel Bustamante <phatbastard81@yahoo.com>
2017-06-17 03:33:06 +0000

    added castle logic to the King.rb and added boolean for valid_move? if castle

--- app/models/king.rb
+++ app/models/king.rb
@@ -10,7 +10,15 @@ after_create :icon
   end
 
   def valid_move?(to_x, to_y)
+    return true if self.castle(to_x, to_y)
     horizontal_move_one_square?(to_x) || vertical_move_one_square?(to_y) || diagonal_move_one_square?(to_x, to_y)
   end
-
+  
+  def castle(to_x, to_y, color)
+    rook = piece.find(name: 'Rook', game_id: game.id, color: color)
+    if self.moved? && Rook.moved? && occupied_squares(to_x, to_x) && !self.check?
+      self.update(x_pos: x_pos - 2, y_pos: y_pos) && rook.update(x_pos: x_pos + 2, y_pos: y_pos)
+    end
+  end
+  
 end
