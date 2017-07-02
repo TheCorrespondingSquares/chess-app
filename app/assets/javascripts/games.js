5dab8da2aa0889fe4c66572fb8ac7c065725b9d1
jwmunn <contact@justinmunn.co>
2017-07-01 19:33:24 -0800

    Refactored get requests.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,30 +1,22 @@
 $(function() {
   var selectedPieceId = null;
   var isPieceSelected = false;
-  var previousSquare;
-  var grabPiece;
-  var draggedPieceId;
+  var selectedSquare;
+  
 
   var selectedPiece;
+  var selectedPieceDiv;
   var destinationPiece;
   var destinationPieceId;
 
-  var board = $( "#board" );
+  var gameId = $('#gameId').data('gameId');
 
   $( ".piece" ).draggable({
-    containment: board,
+    containment: $( "#board" ),
     start: function(e) {
-        draggedPieceId = $(this).data('pieceId');
-        grabPiece = $('.piece[data-piece-id="' + draggedPieceId + '"]');
-        console.log("Piece dragged: " + draggedPieceId);
+        selectedPieceId = $(this).data('pieceId');
+        selectedPieceDiv = $('.piece[data-piece-id="' + selectedPieceId + '"]');
         $(this).parent().addClass('active');
-
-        $.get('/games/' + gameId +'/pieces/' + draggedPieceId).success( function( data ) {
-          selectedPiece = data;
-          // console.log( "Data ID is: " + selectedPiece.id);
-          // console.log( "Is captured?: " + selectedPiece.captured);
-          // console.log( "Color: " + selectedPiece.color);
-        });
     }
   });
   $( "#board td" ).droppable({
@@ -37,37 +29,34 @@ $(function() {
       var $this = $(this);
       var xPos = $this.data('xPos');
       var yPos = $this.data('yPos');
-      var gameId = $('#gameId').data('gameId');
-
-      var clickedSquare = $('td[data-x-pos="' + xPos + '"][data-y-pos="' + yPos + '"]');
-     
       destinationPieceId = $(this).children().data('pieceId');
 
-      if (destinationPieceId != undefined) {
-        $.get('/games/' + gameId +'/pieces/' + destinationPieceId).success( function( data ) {
-          console.log( "Destination data: " + data);
-          destinationPiece = data;
+      getJsonData(gameId, selectedPieceId, function(output) {
+        selectedPiece = output;
+      });
 
-          console.log( "Selected piece color: " + selectedPiece.color);
-          console.log( "Destination piece color: " + destinationPiece.color);
-          if (destinationPiece.color === selectedPiece.color) {
-            console.log( grabPiece );
-            var revertPiece = grabPiece.css({ top: 0, left: 0 }).detach();
-            console.log( revertPiece[0] );
-            var prevSquare = $('td[data-x-pos="' + selectedPiece.x_pos + '"][data-y-pos="' + selectedPiece.y_pos + '"]');
-            console.log( "prevSquare:" + selectedPiece.x_pos + "y: " + selectedPiece.y_pos );
-            $(prevSquare).append(revertPiece);
-          } else {
-            submitMove();
-          }
+      if (destinationPieceId != undefined) {
+        getJsonData(gameId, destinationPieceId, function(output) {
+          destinationPiece = output;
         });
+
+        if (destinationPiece.color === selectedPiece.color) {
+          var revertPiece = selectedPieceDiv.css({ top: 0, left: 0 }).detach();
+          var prevSquare = $('td[data-x-pos="' + selectedPiece.x_pos + '"][data-y-pos="' + selectedPiece.y_pos + '"]');
+          
+          $(prevSquare).append(revertPiece);
+        } else {
+          submitMove();
+        }
+        console.log("Destination: " + destinationPiece.id + " " + destinationPiece.name + " " + destinationPiece.color + " " );
+        console.log("Selected: " + selectedPiece.id + " " + selectedPiece.name + " " + selectedPiece.color + " " );
       } else {
         submitMove();
       }
       
       function submitMove() {
         $.ajax({
-          url: '/games/' + gameId +'/pieces/' + draggedPieceId + '?x_pos=' + xPos + '&y_pos=' + yPos,
+          url: '/games/' + gameId +'/pieces/' + selectedPieceId + '?x_pos=' + xPos + '&y_pos=' + yPos,
           type: 'PUT',
           success: function(data) {
             console.log("AJAX Called");
@@ -76,24 +65,22 @@ $(function() {
 
             console.log( "Destination Piece ID: " + destinationPieceId );
             if ( destinationPiece != undefined) {
+              if (destinationPiece.color == selectedPiece.color) {
+                destinationPiece = undefined;
+              } else {
                 var capturedPiece = $('.piece[data-piece-id="' + destinationPiece.id + '"]').detach();
-                console.log( "Captured Piece: " + destinationPiece.color + destinationPiece.name + "id#" + destinationPiece.id )
+                console.log( "Captured Piece: " + destinationPiece.color + destinationPiece.name + "id#" + destinationPiece.id );
+              }   
             }
-
-            var pieceToMove = grabPiece.detach();
-            // console.log("pieceToMove: " + pieceToMove);
+            var pieceToMove = selectedPieceDiv.detach();
             selectedPieceId = null;
             isPieceSelected = false;
-            console.log("grabPiece: " + grabPiece);
+            
             $this.append(pieceToMove);
-            $( grabPiece ).css({ top: 0, left: 0 });
+            $( selectedPieceDiv ).css({ top: 0, left: 0 });
           }
         });
       }
-      
-
-      console.log("Dropped on Square: " + xPos + " " + yPos);
-      console.log("piece: " + draggedPieceId);
     }
   
 
@@ -101,68 +88,59 @@ $(function() {
     var pieceId = $(this).children('.piece').data('pieceId');
     var xPos = $(this).data('xPos');
     var yPos = $(this).data('yPos');
-    var gameId = $('#gameId').data('gameId');
-
     var clickedSquare = $('td[data-x-pos="' + xPos + '"][data-y-pos="' + yPos + '"]');
     
     console.log("Clicked");
 
     if (isPieceSelected === true && selectedPieceId !== null && selectedPieceId !== pieceId ) {
-      var pieceToMove = grabPiece.detach();
-      console.log("pieceToMove set:");
-      console.log(pieceToMove);
-
+      var pieceToMove = selectedPieceDiv.detach();
       destinationPieceId = $(this).children().data('pieceId');
 
-      if (destinationPieceId != undefined) {
-        $.get('/games/' + gameId +'/pieces/' + destinationPieceId).success( function( data ) {
-          console.log( "Destination data: " + data);
-          destinationPiece = data;
+      getJsonData(gameId, destinationPieceId, function(output) {
+        destinationPiece = output;
+      });
+      
 
-          console.log( "Selected piece color: " + selectedPiece.color);
-          console.log( "Destination piece color: " + destinationPiece.color);
-          if (destinationPiece.color === selectedPiece.color) {
-            console.log( grabPiece );
-            var revertPiece = grabPiece.css({ top: 0, left: 0 }).detach();
-            console.log( revertPiece[0] );
-            var prevSquare = $('td[data-x-pos="' + selectedPiece.x_pos + '"][data-y-pos="' + selectedPiece.y_pos + '"]');
-            console.log( "prevSquare:" + selectedPiece.x_pos + "y: " + selectedPiece.y_pos );
-            $(prevSquare).append(revertPiece);
-          } else {
-            clickSendMove();
-          }
-        });
+      if (destinationPieceId != undefined) {
+        if (destinationPiece.color === selectedPiece.color) {
+          var revertPiece = selectedPieceDiv.css({ top: 0, left: 0 }).detach();
+          selectedSquare = $('td[data-x-pos="' + selectedPiece.x_pos + '"][data-y-pos="' + selectedPiece.y_pos + '"]');
+
+          $(selectedSquare).append(revertPiece);
+        } else {
+          clickSendMove();
+        }
       } else {
        clickSendMove();
-      }
-
-      
+      }      
     } else {
       if (pieceId !== undefined && pieceId !== null) {
+        // Remove 'active' class if td.active is clicked
         if ($(this).hasClass('active')) {
           $(this).removeClass('active');
           selectedPieceId = null;
           isPieceSelected = false;
         } else {
-          $(this).addClass('active');
-          previousSquare = $(this);
+          selectedSquare = $(this);          
           selectedPieceId = pieceId;
           isPieceSelected = true;
-          
-          grabPiece = $('.piece[data-piece-id="' + pieceId + '"]');
-          console.log("grabPiece set:");
-          console.log(grabPiece);
 
-          $.get('/games/' + gameId +'/pieces/' + pieceId).success( function( data ) {
-            selectedPiece = data;
-            // console.log( "Data ID is: " + selectedPiece.id);
-            // console.log( "Is captured?: " + selectedPiece.captured);
-            // console.log( "Color: " + selectedPiece.color);
+          selectedSquare.addClass('active');
+          
+          selectedPieceDiv = $('.piece[data-piece-id="' + pieceId + '"]');
+          console.log("selectedPieceDiv set:");
+          console.log(selectedPieceDiv);
+
+          // selectedPiece = getJsonData(pieceId);
+          getJsonData(gameId, pieceId, function(output) {
+            selectedPiece = output;
           });
+          
         }
       }
     }
 
+    submitMove(gameId, selectedPieceId, xPos, yPos);
     function clickSendMove(){
         $.ajax({
           url: '/games/' + gameId +'/pieces/' + selectedPieceId + '?x_pos=' + xPos + '&y_pos=' + yPos,
@@ -177,10 +155,40 @@ $(function() {
             selectedPieceId = null;
             isPieceSelected = false;
 
-            clickedSquare.append(grabPiece);
-            // location.reload();
+            clickedSquare.append(selectedPieceDiv);
           }
         });
       }
   });
 });
+
+function submitMove(gameId, pieceId, xPos, yPos) {
+  $.ajax({
+    url: '/games/' + gameId +'/pieces/' + selectedPieceId + '?x_pos=' + xPos + '&y_pos=' + yPos,
+    type: 'PUT',
+    success: function(data) {
+      if ( destinationPiece != undefined) {
+        var capturedPiece = $('.piece[data-piece-id="' + destinationPiece.id + '"]').detach();
+        console.log( "Captured Piece: " + destinationPiece.color + destinationPiece.name + "id#" + destinationPiece.id )
+      }
+
+      $('#board td').removeClass('active');
+      selectedPieceId = null;
+      isPieceSelected = false;
+
+      clickedSquare.append(selectedPieceDiv);
+    }
+  });
+}
+
+function getJsonData(gameId, pieceId, handleData) {
+  $.ajax({
+    type: 'GET',
+    url: '/games/' + gameId +'/pieces/' + pieceId,
+    dataType: 'json',
+    success: function( data ) {
+      handleData( data );
+    },
+    async: false
+  });
+}
