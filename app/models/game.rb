359583c65e11ddf80445ebe95c53833549231ea3
Fisto Riza Satianto <fistoriza@me.com>
2017-07-04 14:13:14 +0000

    revised game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -7,23 +7,22 @@ class Game < ApplicationRecord
 	
 	scope :available,	-> { where('white_player_id is NULL OR black_player_id is NULL') }
 	
-	def populate_board!
-	  # ------ Black pieces ----------
-		8.times do |i|
-	  	Pawn.create(color: 'Black', game_id: self.id, x_pos: i, y_pos: 1, icon:"&#9817;")
-		end
-		big_pieces.each_with_index do |piece, i|
-			Piece.create(name: piece, color: 'Black', game_id: self.id, x_pos: i, y_pos: 0)
-		end
-
-		# ------ White pieces -----------
-	  8.times do |i|
-	  	Pawn.create(color: 'White', game_id: self.id, x_pos: i, y_pos: 6, icon: "&#9823;")
-	  end
-		big_pieces.each_with_index do |piece, i|
-			Piece.create(name: piece, color: 'White', game_id: self.id, x_pos: i, y_pos: 7)
-		end
-	end
+  def populate_board!
+    # ------ White pieces -----------
+    8.times do |i|
+      Pawn.create(color: 'White', game_id: self.id, x_pos: i, y_pos: 1, icon: "&#9817;")
+    end
+    big_pieces.each_with_index do |piece, i|
+      Piece.create(name: piece, color: 'White', game_id: self.id, x_pos: i, y_pos: 0)
+    end
+    # ------ Black pieces ----------
+    8.times do |i|
+      Pawn.create(color: 'Black', game_id: self.id, x_pos: i, y_pos: 6, icon:"&#9823;")
+    end
+    big_pieces.each_with_index do |piece, i|
+      Piece.create(name: piece, color: 'Black', game_id: self.id, x_pos: i, y_pos: 7)
+    end
+  end
 
 	def big_pieces
 		%w(Rook Knight Bishop Queen King Bishop Knight Rook)
