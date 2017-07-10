86abc330eed04fb1a6ac1f521a24135cc6961b24
jwmunn <contact@justinmunn.co>
2017-07-09 16:48:27 -0800

    Updated dashboard to show my games.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,7 @@
 class DashboardsController < ApplicationController
   def index
-    @available_games = Game.available
-    @games = @available_games.where.not(black_player_id: current_user).or(@available_games.where.not(white_player_id: current_user))
+    @games = Game.all
+    @available_games = Game.available 
+    @my_games = Game.where('white_player_id = ? or black_player_id = ?', current_user.id, current_user.id).order('updated_at DESC')
   end
 end
