aee085c123748128cc9a9f5b1e4f4c94f1fff2ef
kboygit <heybisayangdako@gmail.com>
2017-07-10 21:23:45 +0000

    added fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Game.delete_all
+User.delete_all
+Piece.delete_all
+
+default_user = User.new(
+  email: "default@gmail.com",
+  nickname: "John Doe",
+  password: "password",
+  password_confirmation: "password"
+)
+# user.skip_confirmation!
+default_user.save!
+
+30.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    nickname: Faker::Internet.user_name,
+    password: "password",
+    password_confirmation: "password"
+  )
+  # user.skip_confirmation!
+  user.save!
+end
