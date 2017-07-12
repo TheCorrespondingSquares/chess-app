789a774600ecbf9bf8c40ea0aed0468d0e4a972a
MiguelBee <MiguelBee@users.noreply.github.com>
2017-07-12 15:19:21 -0600

    Pawn promotion (#76)
    * added can_promote? and pawn_promotion
    
    * refactored
    
    * more refactoring
    
    * refactored for readability
    
    * added more tests to can promote
    
    * added modal and form for pawn promotion
    
    * added pawns_can_promote? method to game.rb
    
    * attempting reroute of simple_form
    
    * pieces_controller methods for pawn promotion
    
    * rendering template instead of partial
    
    * refactored pawn prmote logic and hardcoded queen
    
    * Refactored pawn promotion, moved most of it to pieces_controller
    
    * Fixed expected alert in pieces controller Check? test
    
    * got pawn promotion working with hardcoded Queen

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -12,7 +12,12 @@ class Pawn < Piece
     end
   end
 
+  def can_promote?(y_pos)
+    reached_opposite_border?(y_pos)
+  end  
+  
   private
+
 
   def pawn_move_vertical?(to_x, to_y)
     pawn_move_forward?(to_y) && !is_on_square?(to_x, to_y) && pawn_first_move?(to_y)
@@ -31,7 +36,7 @@ class Pawn < Piece
   end
 
   def pawn_move_forward?(to_y)
-    if self.color == "White"
+    if is_white?
       to_y > starting_point_y
     else
       to_y < starting_point_y
@@ -39,11 +44,23 @@ class Pawn < Piece
   end
 
   def pawn_first_move?(to_y)
-    if self.color == 'White'
+    if is_white?
       self.y_pos == 1 ? to_y < 4 : vertical_move_one_square?(to_y)
     else
       self.y_pos == 6 ? to_y > 3 : vertical_move_one_square?(to_y)
     end
+  end
+  
+  def reached_opposite_border?(y_pos)
+    white_reached_border?(y_pos) || black_reached_border?(y_pos)
+  end
+  
+  def white_reached_border?(y_pos)
+    y_pos == 7 && is_white?
+  end
+  
+  def black_reached_border?(y_pos)
+    y_pos == 0 && is_black?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_facebook_session
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'games#index'  
     resources :games do
-    	resources :pieces, only: [:show, :update]
+    	resources :pieces
     end
     resources :dashboards, only: :index
     root 'games#index'
