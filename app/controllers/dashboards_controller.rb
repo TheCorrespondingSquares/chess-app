3a5b24a6c516b233be84100ffa432065dfe80bad
Fisto Riza Satianto <fistoriza@me.com>
2017-07-15 13:44:45 +0000

    fixed tests

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,17 +1,9 @@
 class DashboardsController < ApplicationController
-<<<<<<< HEAD
-  def index
-    @available_games = Game.available
-    @games = @available_games.where.not(black_player_id: current_user).or(@available_games.where.not(white_player_id: current_user))
-=======
-  before_action :authenticate_user!
-
   def index
     @games = Game.all
     @available_games = Game.available 
     if user_signed_in?
       @my_games = Game.where('white_player_id = ? or black_player_id = ?', current_user.id, current_user.id).order('updated_at DESC')
     end
->>>>>>> c4e8379fa23c67baed3d173f2f1a977ba399adb1
   end
 end
